cff2a9a41680a8d7f9a6c42c924258e9cb25d704
RaihanAk <raihandoank@ymail.com>
2021-05-16 12:06:03 +0700

    init commit

---
+++ Assets/FPS/Scripts/VR Script/CubePointerTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePointerTest : MonoBehaviour
+{
+    Color oriPurple;
+
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = oriPurple;
+    }
+
+    public void ChangeRed()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void ChangeCyan()
+    {
+        GetComponent<Renderer>().material.color = Color.cyan;
+    }
+
+    public void ChangeOriPurple()
+    {
+        GetComponent<Renderer>().material.color = oriPurple;
+    }
+}
---
+++ Assets/FPS/Scripts/VR Script/GameManagerMitsuketa.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerMitsuketa : MonoBehaviour
+{
+    public float dueTime;
+    public int score = 0;
+
+    public bool isStationComplete = false;
+
+    public Text timerText,
+        scoreText,
+        timesUpText;
+    public AudioSource finishBeep;
+
+    void FixedUpdate()
+    {
+        if (!isStationComplete)
+        {
+            dueTime -= Time.deltaTime;
+            timerText.text = TimePassedString();
+            scoreText.text = "Score : " + score;
+
+            // if time runs dead
+            if (dueTime <= 0)
+                EndGame();
+        }
+    }
+
+    public string TimePassedString()
+    {
+        int sec = (int)dueTime % 60,
+            min = (int)dueTime / 60;
+
+        return (min + ":" + sec);
+    }
+
+    public void EndGame()
+    {
+        isStationComplete = true;
+        finishBeep.Play();
+
+        timesUpText.gameObject.SetActive(true);
+        timerText.gameObject.SetActive(false);
+    }
+
+    public void IncrementScore(int n)
+    {
+        score += n;
+    }
+}
